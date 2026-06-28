fc882d50315af89d6c2c67a6eb72a3a713284cd1
raydanielg <airezra2+1@gmail.com>
2026-06-28 15:03:30 +0300

    commit

--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -2,14 +2,31 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Model;
 
 class Payroll extends Model
 {
+    use BelongsToCompany;
+
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'basic_salary' => 'decimal:2',
+        'allowances' => 'decimal:2',
+        'deductions' => 'decimal:2',
+        'net_salary' => 'decimal:2',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 
     public function employee()
     {
         return $this->belongsTo(Employee::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 }
