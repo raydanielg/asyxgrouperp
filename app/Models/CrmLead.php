10e97c2b6e1444a2ab97a6c644df004825365a14
raydanielg <airezra2+1@gmail.com>
2026-06-28 12:12:21 +0300

    commit

--- app/Models/CrmLead.php
+++ app/Models/CrmLead.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Model;
 
 class CrmLead extends Model
 {
+    use BelongsToCompany;
+
     protected $guarded = ['id'];
 
     public function deals()
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use BelongsToCompany;
+
     protected $guarded = ['id'];
 
     protected $casts = ['is_active' => 'boolean'];
