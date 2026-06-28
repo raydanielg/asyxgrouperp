004effbb98177990cb163cb8bef51707b4b74edc
raydanielg <airezra2+1@gmail.com>
2026-06-28 13:12:22 +0300

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserRoleSeeder::class,
             ApprovalWorkflowSeeder::class,
+            MasterDataSeeder::class,
         ]);
     }
 }
