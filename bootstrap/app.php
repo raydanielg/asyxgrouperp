b976d1e278940f610a69063944c78be2a393d99d
raydanielg <airezra2+1@gmail.com>
2026-06-28 12:16:59 +0300

    commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'audit' => \App\Http\Middleware\AuditLogMiddleware::class,
         ]);
+        $middleware->appendToGroup('web', \App\Http\Middleware\AuditLogMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
