da3ac7bb0583d319c0e86632f62b3b14793e15df
raydanielg <airezra2+1@gmail.com>
2026-06-28 12:14:20 +0300

    commit

--- app/Models/ApprovalWorkflow.php
+++ app/Models/ApprovalWorkflow.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ApprovalWorkflow extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToCompany;
 
     protected $fillable = [
         'name', 'module', 'description', 'is_active', 'company_id', 'created_by',
--- app/Models/Document.php
+++ app/Models/Document.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToCompany;
 
     protected $fillable = [
         'document_number', 'title', 'description', 'category', 'file_path',
