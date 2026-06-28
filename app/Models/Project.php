5f89f292911da19a158a49c0c14c0d7e0e3ff001
raydanielg <airezra2+1@gmail.com>
2026-06-28 12:10:55 +0300

    commit

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    use BelongsToCompany;
+
     protected $guarded = ['id'];
 
     protected $casts = [
