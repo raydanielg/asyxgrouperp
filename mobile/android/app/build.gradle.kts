aeae94d495f2109c2f96b853b1b1a73948b646e0
raydanielg <airezra2+1@gmail.com>
2026-06-29 08:44:49 +0300

    commit

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -5,18 +5,19 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mobile"
+    namespace = "com.asyxgroup.erp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mobile"
+        applicationId = "com.asyxgroup.erp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -32,6 +33,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 kotlin {
