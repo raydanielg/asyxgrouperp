8d589ba2183d229c786f0f25e902953189e89f33
raydanielg <airezra2+1@gmail.com>
2026-07-01 10:36:08 +0300

    commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -219,7 +219,7 @@
                     'admin.office-expenses*' => 'view-expenses',
                     'admin.client-receipts*' => 'view-revenues',
                     'admin.meetings*' => 'view-dashboard',
-                    'settlements*' => 'view-projects',
+                    'admin.settlements*' => 'view-projects',
                     'admin.users*' => 'view-users',
                     'admin.roles*' => 'view-roles',
                     'admin.users.login-history*' => 'view-login-history',
