e18a876e265cf86637b5d475e690b96303567517
raydanielg <airezra2+1@gmail.com>
2026-07-03 03:57:42 +0300

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, \App\Traits\BelongsToCompany;
 
     /**
      * Get the attributes that should be cast.
