0c26b1c7034fbbb41a7b30f8f226264c7696bd38
raydanielg <airezra2+1@gmail.com>
2026-07-03 04:45:38 +0300

    commit

--- app/Http/Controllers/RoleDashboardController.php
+++ app/Http/Controllers/RoleDashboardController.php
@@ -447,13 +447,13 @@ class RoleDashboardController extends Controller
                 ['label' => 'Total Contracts', 'value' => $stats['totalContracts'] ?? 0, 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2', 'color' => 'sky'],
                 ['label' => 'Active Projects', 'value' => $stats['activeProjects'] ?? 0, 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2', 'color' => 'amber'],
             ],
-            'receptionist', 'call_center_agent' => [
+            'receptionist', 'call_center_agent', 'sales_manager', 'business_development_manager', 'sales_executive', 'crm_officer', 'marketing_officer', 'call_center_supervisor' => [
                 ['label' => 'New Leads', 'value' => $stats['newLeads'] ?? 0, 'icon' => 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6', 'color' => 'emerald'],
                 ['label' => 'Total Leads', 'value' => $stats['totalLeads'] ?? 0, 'icon' => 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z', 'color' => 'sky'],
                 ['label' => 'Open Tickets', 'value' => $stats['openTickets'] ?? 0, 'icon' => 'M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0z', 'color' => 'amber'],
                 ['label' => 'Total Contacts', 'value' => $stats['totalContacts'] ?? 0, 'icon' => 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z', 'color' => 'emerald'],
             ],
-            'logistics_officer' => [
+            'logistics_officer', 'procurement_manager', 'procurement_officer', 'tender_officer', 'store_manager', 'storekeeper', 'inventory_controller', 'asset_officer' => [
                 ['label' => 'Total Products', 'value' => $stats['totalProducts'] ?? 0, 'icon' => 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4', 'color' => 'emerald'],
                 ['label' => 'Low Stock', 'value' => $stats['lowStockProducts'] ?? 0, 'icon' => 'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z', 'color' => 'amber'],
                 ['label' => 'Warehouses', 'value' => $stats['totalWarehouses'] ?? 0, 'icon' => 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5', 'color' => 'sky'],
