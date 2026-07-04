e1384fbb63206535b88201e955ce292971739c92
raydanielg <airezra2+1@gmail.com>
2026-07-04 04:42:46 +0300

    commit

--- temp_list_perms.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$perms = App\Models\Permission::orderBy('module')->orderBy('name')->limit(200)->get();
-foreach ($perms as $p) {
-    echo $p->module . '/' . $p->name . PHP_EOL;
-}
