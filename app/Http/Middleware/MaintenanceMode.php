3c5a9286db4d15144b2ed5b7d5c615c31f2bec86
raydanielg <airezra2+1@gmail.com>
2026-07-04 03:39:04 +0300

    commit

---
+++ app/Http/Middleware/MaintenanceMode.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class MaintenanceMode
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (config('app.maintenance_mode', false) && !auth()->check()) {
+            return response()->view('errors.maintenance', [], 503);
+        }
+
+        if (config('app.maintenance_mode', false) && auth()->check()) {
+            $user = auth()->user();
+            $allowed = $user->role === 'admin' || $user->hasRole('admin') || $user->hasRole('erp_super_administrator');
+            if (!$allowed) {
+                auth()->logout();
+                return response()->view('errors.maintenance', [], 503);
+            }
+        }
+
+        return $next($request);
+    }
+}
