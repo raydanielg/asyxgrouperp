66c0b86976ecb2b83e9c40b8a10c339837300741
raydanielg <airezra2+1@gmail.com>
2026-07-09 00:31:00 +0300

    commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -155,6 +155,7 @@
                         ['label' => 'Meetings', 'route' => 'admin.meetings.index', 'icon' => 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zM6 3v2m12-2v2M3 8h2m14 0h2', 'match' => 'admin.meetings*'],
                         ['label' => 'Settlements', 'route' => 'admin.settlements.index', 'icon' => 'M9 17v-2m2 2v-4m2 4v-6m2 6V7m2 10V5M3 7l3-3 3 3M3 17l3 3 3-3', 'match' => 'admin.settlements*'],
                         ['label' => 'Job Cards', 'route' => 'admin.job-cards.index', 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6 4h6', 'match' => 'admin.job-cards*'],
+                        ['label' => 'Contracts', 'route' => 'admin.contracts.index', 'icon' => 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z', 'match' => 'admin.contracts*'],
                     ]],
                     ['title' => 'Products & Inventory', 'items' => [
                         ['label' => 'Products', 'route' => 'admin.products.index', 'icon' => 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4', 'match' => 'admin.products*'],
