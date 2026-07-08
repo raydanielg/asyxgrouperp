8d981a61403142e5aec7d592eca88a099a39450d
raydanielg <airezra2+1@gmail.com>
2026-07-09 00:26:19 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -758,6 +758,10 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'route-permission'])
     Route::get('/projects/{project}/financing', [$ext, 'projectFinancingIndex'])->name('projects.financing');
     Route::post('/projects/{project}/financing', [$ext, 'projectFinancingStore'])->name('projects.financing.store');
     Route::post('/financing/{financing}/repayment', [$ext, 'projectFinancingRepaymentStore'])->name('projects.financing.repayment');
+    Route::get('/projects/{project}/expense-requests', [$ext, 'projectExpenseRequestIndex'])->name('projects.expense-requests');
+    Route::post('/projects/{project}/expense-requests', [$ext, 'projectExpenseRequestStore'])->name('projects.expense-requests.store');
+    Route::post('/expense-requests/{expenseRequest}/approve', [$ext, 'projectExpenseRequestApprove'])->name('projects.expense-requests.approve');
+    Route::post('/expense-requests/{expenseRequest}/reject', [$ext, 'projectExpenseRequestReject'])->name('projects.expense-requests.reject');
     Route::get('/settlements', [$ext, 'projectSettlementsIndex'])->name('settlements.index');
     Route::post('/projects/{project}/generate-invoice', [\App\Http\Controllers\Admin\ErpExtendedController::class, 'generateProjectInvoice'])->name('projects.generate-invoice');
     Route::get('/projects/{project}/pdf', [$ext, 'projectPdf'])->name('projects.pdf');
