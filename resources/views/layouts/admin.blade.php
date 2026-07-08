14f89bef08c2799f9a4380077c130afffddfa852
raydanielg <airezra2+1@gmail.com>
2026-07-09 00:16:16 +0300

    commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -475,6 +475,7 @@
                     'technician' => [
                         ['label' => 'Dashboard', 'route' => 'role.dashboard', 'icon' => $iconDashboard, 'match' => 'role.dashboard'],
                         ['label' => 'My Tickets', 'module' => 'tickets', 'icon' => $iconTickets],
+                        ['label' => 'Job Cards', 'module' => 'job-cards', 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4'],
                         ['label' => 'Projects', 'module' => 'projects', 'icon' => $iconProjects],
                         ['label' => 'Timesheets', 'module' => 'timesheets', 'icon' => $iconTimesheets],
                         ['label' => 'Bugs', 'module' => 'bugs', 'icon' => $iconBugs],
